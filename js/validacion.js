6df2ee7d91fbcd15a7263bd4b424b4b20be664d8
Che-Pibe27 <ivannsilveiraa@gmail.com>
2023-08-10 09:38:10 -0300

    Validacion pwd
    coinciden las contraseñas

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,23 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function comprobarPwd () {
+    const password1 = document.getElementById("password1").value
+    const confirmPassword = document.getElementById("confirmPassword").value
+    
+    console.log(password1, confirmPassword)
+    if ( password1 !== confirmPassword ) {
+        console.log("entre al if")        
+        return showAlertError()
+    }
+    else {
+        console.log(showAlertSuccess())
+        return showAlertSuccess()
+
+    }   
+
+}
+
+const registerbtn = document.getElementById("regBtn")
+registerbtn.addEventListener("click", () => comprobarPwd())
